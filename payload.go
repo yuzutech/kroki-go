bfb61c4bdc7a880b7f42d7916de2bd94617a5eda
arukiidou <arukiidou@yahoo.co.jp>
2024-03-24 00:32:13 +0900

    remove archived package, use std (#30)

--- payload.go
+++ payload.go
@@ -4,8 +4,7 @@ import (
 	"bytes"
 	"compress/zlib"
 	"encoding/base64"
-
-	"github.com/pkg/errors"
+	"fmt"
 )
 
 // CreatePayload takes a string and returns a payload in deflate + base64 format
@@ -14,12 +13,12 @@ func CreatePayload(input string) (string, error) {
 	var buffer bytes.Buffer
 	writer, err := zlib.NewWriterLevel(&buffer, 9)
 	if err != nil {
-		return "", errors.Wrap(err, "fail to create the writer")
+		return "", fmt.Errorf("fail to create the writer: %w", err)
 	}
 	_, err = writer.Write([]byte(input))
 	writer.Close()
 	if err != nil {
-		return "", errors.Wrap(err, "fail to create the payload")
+		return "", fmt.Errorf("fail to create the payload: %w", err)
 	}
 	result := base64.URLEncoding.EncodeToString(buffer.Bytes())
 	return result, nil
